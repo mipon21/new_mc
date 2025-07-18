cad924a347e99f3044169c856f47ec4777ce580f
mipon21 <mipon5500u@gmail.com>
2025-07-18 11:12:59 +0600

    stable

--- platform/packages/plugin-management/config/general.php
+++ platform/packages/plugin-management/config/general.php
@@ -2,7 +2,7 @@
 
 return [
     'enable_plugin_manager' => env('CMS_PLUGIN_ENABLE_PLUGIN_MANAGER', true),
-    'hide_plugin_author' => env('CMS_PLUGIN_HIDE_AUTHOR', false),
+    'hide_plugin_author' => env('CMS_PLUGIN_HIDE_AUTHOR', true),
     'enable_plugin_list_cache' => env('CMS_PLUGIN_ENABLE_PLUGIN_LIST_CACHE', false),
-    'enable_marketplace_feature' => env('CMS_ENABLE_MARKETPLACE_FEATURE', true),
+    'enable_marketplace_feature' => env('CMS_ENABLE_MARKETPLACE_FEATURE', false),
 ];
